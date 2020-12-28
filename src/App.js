b8825a3d461a773df76bdeb3846004c1017bbf29
Alvey <alvey1504005@gmail.com>
2020-12-29 01:08:03 +0600

    Transformed homepage into resusable react component styles the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.scss';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      
     </div>
   );
 }
